21d2e7ddaa5b02c471d90561d5ba28ac1bb72331
Salim Rahman <me@salimrahman.com>
2023-12-07 19:13:17 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,6 +22,7 @@ runSystemUpdates() {
     apt update
     sleep 3 && clear && echo -e "\n\t${DARK_GRAY}Updating System...${NoColor}" && sleep 3
     apt upgrade -y
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 }
 
 # Installing latest version of Docker and DockerCompose
@@ -33,7 +34,9 @@ installDocker() {
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     apt-cache policy docker-ce
     sleep 3 && clear && echo -e "\n\t${DARK_GRAY}Installing Docker and Docker-Compose...${NoColor}" && sleep 3
-    apt install docker-ce docker-compose -y
+    apt install docker-ce -y
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 }
 
 # Installing latest version of Portainer
